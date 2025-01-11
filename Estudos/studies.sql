60cd22b766c7e4347c8c7e19cbfd9d19657bed55
João Emanuel Pessoa do Nascimento <joao.emanuel.pn@gmail.com>
2025-01-11 11:55:53 -0300

    Create studies.sql

---
+++ Estudos/studies.sql
@@ -0,0 +1,17 @@
+create table employees (
+employee_id INT,
+first_name VARCHAR(50),
+last_name VARCHAR(50),
+hourly_pay DECIMAL(5,2),
+hire_date DATE
+);
+
+
+
+alter table employees
+add phone_number VARCHAR(15)
+
+select * from employees
+
+alter table employees
+MODIFY COLUMN email VARCHAR(15);
